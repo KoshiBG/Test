eb72b18d7362409f33185a6446020f1d27b01f70
KoshiBG <97697037+KoshiBG@users.noreply.github.com>
2022-09-14 23:47:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ x = int(input("What is Bitcoin price today?"))
 print(x)
 y = int(input("How much $ do you have?"))
 print(y)
-z = x / y
-print(f"You can buy {z} BTC")
+print(f"You can buy {y / x} BTC")
